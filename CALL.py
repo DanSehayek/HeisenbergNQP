417c278da075c43ef7ef10101d6e61659d557dd8
DanSehayek <dsehayek@uwaterloo.ca>
2018-10-07 09:42:29 -0400

    Added H check

--- CALL.py
+++ CALL.py
@@ -4,14 +4,23 @@ from OperatorCheck import operatorCheck
 def RUN(check,numQubits):
     if check == "Frequency":
         countCheck("Samples/{0}Q".format(numQubits))
-    elif check == "Operator":
+    elif check == "S2S3":
         if numQubits == 3:
-            operatorCheck("Samples/3Q",-0.166667,range(100,10000,100),3)
+            operatorCheck("S2S3","Samples/3Q",-0.166667,range(100,10000,100),3)
         elif numQubits == 4:
-            operatorCheck("Samples/4Q",-0.0833333,range(100,10000,100),4)
+            operatorCheck("S2S3","Samples/4Q",-0.0833333,range(100,10000,100),4)
         elif numQubits == 5:
-            operatorCheck("Samples/5Q",-0.127916,range(100,10000,100),5)
+            operatorCheck("S2S3","Samples/5Q",-0.127916,range(100,10000,100),5)
         elif numQubits == 10:
-            operatorCheck("Samples/10Q",-0.0961352,range(100,10000,100),10)
+            operatorCheck("S2S3","Samples/10Q",-0.0961352,range(100,10000,100),10)
+    elif check == "H":
+        if numQubits == 3:
+            operatorCheck("H","Samples/3Q",-1.000000,range(100,10000,100),3)
+        elif numQubits == 4:
+            operatorCheck("H","Samples/4Q",-1.616025,range(100,10000,100),4)
+        elif numQubits == 5:
+            operatorCheck("H","Samples/5Q",-1.927886,range(100,10000,100),5)
+        elif numQubits == 10:
+            operatorCheck("H","Samples/10Q",-4.258035,range(100,10000,100),10)
 
-RUN("Operator",10)
+RUN("H",4)
