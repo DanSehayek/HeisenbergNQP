f06de02237daff8d85d15f992d552e12d82c4014
DanSehayek <dsehayek@uwaterloo.ca>
2018-10-06 22:34:58 -0400

    Added Operator Checks

--- CALL.py
+++ CALL.py
@@ -1,5 +1,13 @@
-import FrequencyCheck
-import OperatorCheck
+from FrequencyCheck import countCheck
+from OperatorCheck import operatorCheck
 
-#FrequencyCheck.countCheck("Samples/4Q")
-OperatorCheck.operatorCheck("Samples/4Q",-0.0833333,range(1000,10000,1000),4)
+def RUN(check,numQubits):
+    if check == "Frequency":
+        countCheck("Samples/{0}Q".format(numQubits))
+    elif check == "Operator":
+        if numQubits == 3:
+            operatorCheck("Samples/3Q",-0.166667,range(100,10000,100),3)
+        elif numQubits == 4:
+            operatorCheck("Samples/4Q",-0.0833333,range(100,10000,100),4)
+        elif numQubits == 5:
+            operatorCheck("Samples/5Q",-0.127916,range(100,10000,100),5)
